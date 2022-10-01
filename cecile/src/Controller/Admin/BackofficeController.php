9a84d9676e1634642abe8af11c669928fe5d9fc3
OrfeoDev <orfeo34@icloud.com>
2022-10-01 19:36:38 +0200

    Tentative d'ameliraation du code

--- cecile/src/Controller/Admin/BackofficeController.php
+++ cecile/src/Controller/Admin/BackofficeController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Contact;
 use App\Repository\ContactRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,11 +17,37 @@ class BackofficeController extends AbstractController
     #[IsGranted('ROLE_ADMIN')]
     public function index(ContactRepository $contactRepository): Response
     {
-      //  $contact = $contactRepository->findBy([], ['nom' => 'ASC']);
+        //  $contact = $contactRepository->findBy([], ['nom' => 'ASC']);
         $contact = $contactRepository->findBy([], ['nom' => 'ASC']);
         //dd($contact);
+
         return $this->render('admin/backoffice/index.html.twig', [
             'contact' => $contact,
         ]);
     }
+
+   // #[Route('/admin/backoffice/{id} ', name: 'admin_modifer')]
+   //{
+      // $contact = $em->getRepository(Contact::class)->find($id);
+
+      //  if (!$contact) {
+
+       //     throw $this->createNotFoundException('pas de nom' . $id);
+     //   }
+
+     //  $em->flush();
+
+       //return $this->redirectToRoute('app_admin_backoffice', ['id' => $contact->getId()]);
+  //  }
+
+    #[Route('/admin/backoffice/{id}/suppression', name: 'admin_suppression')]
+    public function supprimer(EntityManagerInterface $em, ContactRepository $contactRepository, Contact $contact): Response
+    {
+        $em->remove($contact);
+        $em->flush();
+
+        return $this->redirectToRoute('app_admin_backoffice');
+    }
+
+
 }
